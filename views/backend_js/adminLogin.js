517c27c3909c515e71abac802b8dfba52322fe4a
Manjuman Abhishek <maurya.abhishek265@gmail.com>
2019-03-04 23:45:15 +0530

    new

--- views/backend_js/adminLogin.js
+++ views/backend_js/adminLogin.js
@@ -1,27 +1,39 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      alert('d')
-      // ...
-    } else {
-      // User is signed out.
-      alert('error2')
-      // ...
+(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCxj3ui9J8yNR8ggBlcFZyb9hojORs8_wY",
+    authDomain: "gurl-3a50b.firebaseapp.com",
+    databaseURL: "https://gurl-3a50b.firebaseio.com",
+    projectId: "gurl-3a50b",
+    storageBucket: "gurl-3a50b.appspot.com",
+    messagingSenderId: "1077432197158"
+  };
+  firebase.initializeApp(config);
+
+  
+  var userid = document.getElementById('adminLoginUserid')
+  var userpass = document.getElementById('adminLoginPass')
+  var loginBtn = document.getElementById('adminLoginBtn')
+
+  loginBtn.addEventListener('click', e=>{
+
+    const email= userid.value
+    const pass = userpass.value
+
+    const auth = firebase.auth()
+
+    const promise = auth.signInWithEmailAndPassword(email, pass)
+
+    promise.catch(e=>console.log(e.message))
+
+  })
+
+  firebase.auth().onAuthStateChanged(firebaseUser =>{
+    if(firebaseUser){
+      
+    }else{
+      console.log('not logged in')
     }
-    // ...
-  });
+  })
 
-function login(){
-    var userid = document.getElementById('adminLoginUserid').value
-    var userpass = document.getElementById('adminLoginPass').value
-    if(!userid === null && userpass === null)
-    firebase.auth().signInWithEmailAndPassword(userid, userpass).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code
-    var errorMessage = error.message
-    // ...
-
-    alert('Error---' + errorMessage)
-    })
-
-}
+}())
